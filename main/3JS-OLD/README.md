f8a28c532a6e5db139b0d83b6e6edfa064dead33
Pranav Verma <pranav@verma.net.in>
2023-11-27 16:23:57 +0530

    Added Old 3JS Files

---
+++ main/3JS-OLD/README.md
@@ -0,0 +1,4 @@
+# 3js-Web
+A 3JS Test Web Page
+
+https://3js-test.web.app
---
+++ main/3JS-OLD/src/main.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue';
+import ThreeScene from './ThreeScene.vue';
+
+createApp(ThreeScene).mount('#app');
