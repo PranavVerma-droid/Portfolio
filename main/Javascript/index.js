c6fc225d8deb0bcf2f4139b8400aad378791e416
Pranav Verma <pranav@verma.net.in>
2024-05-01 15:45:38 +0530

    Fixed Animations using Animate.css

--- main/Javascript/index.js
+++ main/Javascript/index.js
@@ -1,71 +1,71 @@
 function showabout(){
     $("#about_container").css("display","inherit");
-    $("#about_container").addClass("animated slideInLeft");
+    $("#about_container").addClass("animate__animated animate__slideInLeft");
     setTimeout(function(){
-        $("#about_container").removeClass("animated slideInLeft");
+        $("#about_container").removeClass("animate__animated animate__slideInLeft");
     },800);
 }
 function closeabout(){
-    $("#about_container").addClass("animated slideOutLeft");
+    $("#about_container").addClass("animate__animated animate__slideOutLeft");
     setTimeout(function(){
-        $("#about_container").removeClass("animated slideOutLeft");
+        $("#about_container").removeClass("animate__animated animate__slideOutLeft");
         $("#about_container").css("display","none");
     },800);
 }
 function showwork(){
     $("#work_container").css("display","inherit");
-    $("#work_container").addClass("animated slideInRight");
+    $("#work_container").addClass("animate__animated animate__slideInRight");
     setTimeout(function(){
-        $("#work_container").removeClass("animated slideInRight");
+        $("#work_container").removeClass("animate__animated animate__slideInRight");
     },800);
 }
 function closework(){
-    $("#work_container").addClass("animated slideOutRight");
+    $("#work_container").addClass("animate__animated animate__slideOutRight");
     setTimeout(function(){
-        $("#work_container").removeClass("animated slideOutRight");
+        $("#work_container").removeClass("animate__animated animate__slideOutRight");
         $("#work_container").css("display","none");
     },800);
 }
 function showcontact(){
     $("#contact_container").css("display","inherit");
-    $("#contact_container").addClass("animated slideInUp");
+    $("#contact_container").addClass("animate__animated animate__slideInUp");
     setTimeout(function(){
-        $("#contact_container").removeClass("animated slideInUp");
+        $("#contact_container").removeClass("animate__animated animate__slideInUp");
     },800);
 }
 function closecontact(){
-    $("#contact_container").addClass("animated slideOutDown");
+    $("#contact_container").addClass("animate__animated animate__slideOutDown");
     setTimeout(function(){
-        $("#contact_container").removeClass("animated slideOutDown");
+        $("#contact_container").removeClass("animate__animated animate__slideOutDown");
         $("#contact_container").css("display","none");
     },800);
 }
 
 function showblogs() {
     $("#blogs_container").css("display", "inherit");
-    $("#blogs_container").addClass("slideInDown animated");
+    $("#blogs_container").addClass("animate__slideInDown animate__animated");
     setTimeout(function () {
-        $("#blogs_container").removeClass("slideInDown animated");
+        $("#blogs_container").removeClass("animate__slideInDown animate__animated");
     }, 800);
 }
 
 function closeblogs() {
-    $("#blogs_container").addClass("slideOutUp animated");
+    $("#blogs_container").addClass("animate__slideOutUp animate__animated");
     setTimeout(function () {
         $("#blogs_container").css("display", "none");
-        $("#blogs_container").removeClass("slideOutUp animated");
+        $("#blogs_container").removeClass("animate__slideOutUp animate__animated");
     }, 800);
 }
 
 setTimeout(function(){
-    $("#loading").addClass("animated fadeOut");
+    $("#loading").addClass("animate__animated animate__fadeOut");
     setTimeout(function(){
-      $("#loading").removeClass("animated fadeOut");
+      $("#loading").removeClass("animate__animated animate__fadeOut");
       $("#loading").css("display","none");
       $("#box").css("display","none");
-      $("#about").removeClass("animated fadeIn");
-      $("#contact").removeClass("animated fadeIn");
-      $("#work").removeClass("animated fadeIn");
-      $("#blogs").removeClass("animated fadeIn");
+      $("#about").removeClass("animate__animated animate__fadeIn");
+      $("#contact").removeClass("animate__animated animate__fadeIn");
+      $("#work").removeClass("animate__animated animate__fadeIn");
+      $("#blogs").removeClass("animate__animated animate__fadeIn");
     },1000);
 },1500);
