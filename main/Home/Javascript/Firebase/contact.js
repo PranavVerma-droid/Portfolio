dd5265cb8cc875c44bba1a52eabc84addd5af87e
Pranav Verma <pranav@verma.net.in>
2024-09-15 16:52:49 +0530

    Switched the License to GNU AGPL v3

--- main/Home/Javascript/Firebase/contact.js
+++ main/Home/Javascript/Firebase/contact.js
@@ -1,3 +1,23 @@
+/* 
+    Copyright (C) 2024  Pranav Verma
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    See a more apt description in LICENSE File Attached to the root of this
+    project.
+*/
+
 var config = {
     apiKey: "AIzaSyCA_BPpKq3IhLupHnGYbbwq0U1mLdMbJXY",
     authDomain: "contactusform-f0ec2.firebaseapp.com",
@@ -10,11 +30,9 @@ var config = {
   };
   
   firebase.initializeApp(config);
-  
-  // Reference messages collection
   var messagesRef = firebase.database().ref('forms');
   
-  // Listen for form submit
+  // Add Linstener for Form
   $(document).ready(function() {
       const bclick = document.getElementById('contactForm');
       bclick.addEventListener('submit', submitForm);
@@ -24,33 +42,27 @@ var config = {
   
   //const bclick = document.getElementById('contactForm').addEventListener('submit', submitForm);
   
-  // Submit form
+  // Form Submission
   function submitForm(e){
     e.preventDefault();
   
-    //Get value
     var name = getInputVal('name');
     var company = ""
     var email = getInputVal('email');
     var phone = ""
     var message = getInputVal('message');
   
-    // Save message
     saveMessage(name, company, email, phone, message);
   
   
-    // Clear form
     document.getElementById('contactForm').reset();
-    //send alert
     alert("Thank you for your message! I will get back to you as soon as possible");
   }
   
-  // Function to get form value
   function getInputVal(id){
     return document.getElementById(id).value;
   }
-  
-  // Save message to firebase
+
   function saveMessage(name, company, email, phone, message){
     var newMessageRef = messagesRef.push();
     newMessageRef.set({
--- main/Home/Javascript/Firebase/works.js
+++ main/Home/Javascript/Firebase/works.js
@@ -1,3 +1,23 @@
+/* 
+    Copyright (C) 2024  Pranav Verma
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    See a more apt description in LICENSE File Attached to the root of this
+    project.
+*/
+
 const firebaseConfig2 = {
     apiKey: "AIzaSyCA_BPpKq3IhLupHnGYbbwq0U1mLdMbJXY",
     authDomain: "contactusform-f0ec2.firebaseapp.com",
@@ -6,8 +26,8 @@ const firebaseConfig2 = {
     storageBucket: "contactusform-f0ec2.appspot.com",
     messagingSenderId: "641931730164",
     appId: "1:641931730164:web:0812ee1bf4659f8381d2a1",
-    measurementId: "G-1RVB7HZQWB"
-};
+    measurementId: "G-1RVB7HZQWB" };
+    
 firebase.initializeApp(firebaseConfig2);
 const db2 = firebase.firestore();
 
